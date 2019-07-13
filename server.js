40358224cfc7b0605cae74250c8c742240a6b06c
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-07-13 13:02:49 -0500

    new code added to server, app and index, still not full functional(not yet posting to page)

---
+++ server.js
@@ -0,0 +1,91 @@
+//Dependencies
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose")
+//For scraping
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+//Require Article and Note
+var db = require("./models");
+//Port
+var PORT = 3000;
+//Express initialization
+var app = express();
+
+
+app.use(logger("dev"));
+//Make JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/27017"
+)
+
+
+//Scrape 
+app.get("/scrape", function(req, res) {
+  
+    axios.get("https://www.cnn.com").then(function(response) {
+      var $ = cheerio.load(response.data);
+      $("article h2").each(function(i, element) {
+        var result = {};
+        result.title = $(this)
+          .children("a")
+          .text();
+        result.link = $(this)
+          .children("a")
+          .attr("href");
+        db.Article.create(result)
+          .then(function(dbArticle) {
+            console.log(dbArticle);
+          })
+          .catch(function(err) {
+            console.log(err);
+          });
+      });
+      res.send("All done scraping");
+    });
+  });
+//Getting all
+  app.get("/articles", function(req, res) {
+    db.Article.find({})
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+
+  app.get("/articles/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+      .populate("note")
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+  
+// NOTE
+  app.post("/articles/:id", function(req, res) {
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      })
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+  
+//SERVER
+  app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
