2cdc8ae95d200e59c7261c78c7eb2b45ac01a99f
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-07-16 19:42:47 -0500

    reinstall dependencies to make it run on heroku?

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static("public"));
 //If on heroku use deployed database
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI );
 
 
 //Scrape 
